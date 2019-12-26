34d2b3c791d00b43410e4e3fbfd82ad0516801e7
Sam Wolfs <swo@pop-os.localdomain>
2019-12-26 15:34:07 +0100

    Chapter 9

--- grand-theft-wumpus.lisp
+++ grand-theft-wumpus.lisp
@@ -189,3 +189,30 @@
                             (princ new-pos)
                             (handle-new-place nil new-pos nil))))))
 
+;; CHAPTER 9
+
+;; Test performance for a city with 1000 nodes and edges, built 100 times
+(setf *edge-num* 1000)
+(setf *node-num* 1000)
+(defun test-performance ()
+    (time (dotimes (i 100) (get-connected 1 (make-edge-list)))))
+
+;; Use hash tables instead of alists
+(defun hash-edges (edge-list)
+    (let ((tab (make-hash-table)))
+        (mapc (lambda (x)
+                (let ((node (car x)))
+                    (push (cdr x) (gethash node tab))))
+              edge-list)
+        tab))
+
+(defun get-connected-hash (node edge-tab)
+    (let ((visited (make-hash-table)))
+        (labels ((traverse (node)
+                    (unless (gethash node visited)
+                        (setf (gethash node visited) t)
+                        (mapc (lambda (edge)
+                                (traverse edge))
+                              (gethash node edge-tab)))))
+            (traverse node))
+        visited))
